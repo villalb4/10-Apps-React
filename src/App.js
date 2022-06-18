8ce39d4b5048ab3a45904fdff32fe61ceb3c54dd
villalb4 <villalb4angel@gmail.com>
2022-06-18 02:00:49 -0300

    Weather app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Todo from "./Components/Todo/Todo";
+import Weather from "./Components/Weather/Weather";
 import "./App.css"
 
 function App() {
   return (
     <div>
       <Todo />
+      <Weather />
     </div>
   )
 }
